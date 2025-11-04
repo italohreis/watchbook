978a80fcebcb4cd8aa96dbf8159e9564bda15dab
Italo <italohenrikbr@gmail.com>
2025-11-04 10:33:15 -0300

    feat: implement login functionality with user authentication; add login page, routing, and user model

---
+++ watchbook-mobile/src/app/login/login.page.ts
@@ -0,0 +1,112 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import {
+  IonContent,
+  IonLabel,
+  IonInput,
+  IonItem,
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardTitle
+} from '@ionic/angular/standalone';
+import { LoadingController } from '@ionic/angular';
+import { NavController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
+import { CapacitorHttp, HttpOptions, HttpResponse } from '@capacitor/core';
+import { Storage } from '@ionic/storage-angular'; 
+import { Usuario } from './models/usuario.model';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+  standalone: true,
+  imports: [
+    IonContent,
+    IonLabel,
+    IonInput,
+    IonItem,
+    IonButton,
+    IonCard,
+    IonCardContent,
+    IonCardHeader,
+    IonCardTitle,
+    CommonModule,
+    FormsModule
+  ],
+  providers: [Storage]
+})
+export class LoginPage implements OnInit {
+  
+  public instancia: { username: string, password: string } = {
+    username: '',
+    password: ''
+  };
+
+  constructor(
+    public controle_carregamento: LoadingController,
+    public controle_navegacao: NavController,
+    public controle_alerta: AlertController,
+    public controle_toast: ToastController,
+    public storage: Storage
+  ) { }
+
+  async ngOnInit() {
+    await this.storage.create();
+  }
+
+
+  async autenticarUsuario() {
+
+    // Inicializa interface com efeito de carregamento
+    const loading = await this.controle_carregamento.create({message: 'Autenticando...', duration: 15000});
+    await loading.present();
+
+    // Define informações do cabeçalho da requisição
+    const options: HttpOptions = {
+      headers: {'Content-Type': 'application/json'},
+      url: 'http://127.0.0.1:8000/api/login/',
+      data: this.instancia
+    };
+
+    // Autentica usuário junto a API do sistema web
+    CapacitorHttp.post(options)
+      .then(async (resposta: HttpResponse) => {
+
+        // Verifica se a requisição foi processada com sucesso
+        if(resposta.status == 200) {
+
+          // Armazena localmente as credenciais de usuário
+          let usuario: Usuario = Object.assign(new Usuario(), resposta.data);
+          await this.storage.set('usuario', usuario);
+          
+          // Finaliza autenticação e redireciona para interface inicial
+          loading.dismiss();
+          this.controle_navegacao.navigateRoot('/tabs/tab1');
+        }
+        else {
+          // Finaliza autenticação e apresenta mensagem de erro
+          loading.dismiss();
+          this.apresenta_mensagem(resposta.status);
+        }
+      })
+      .catch(async (erro: any) => {
+        console.log(erro);
+        loading.dismiss();
+        this.apresenta_mensagem(erro?.status);
+      });
+  }
+
+  async apresenta_mensagem(codigo: number) {
+    const mensagem = await this.controle_toast.create({
+      message: `Falha ao autenticar usuário: código ${codigo}`,
+      cssClass: 'ion-text-center',
+      duration: 2000
+    });
+    mensagem.present();
+  }
+}
